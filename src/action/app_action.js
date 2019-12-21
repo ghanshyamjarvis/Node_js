97e9a8c8d900f81a5a72cfaacdce99627af5e90c
USR-08 <ghanshyam.jarvis@gmail.com>
2019-12-21 14:25:35 +0530

    workig code

--- src/action/app_action.js
+++ src/action/app_action.js
@@ -1,11 +1,10 @@
 module.exports= {
   main: function (req, res) {
-    var person = [{name: "shyam", id: "001", dept: "computer"},
+    var me = [{name: "shyam", id: "001", dept: "computer"},
       {name: "ram", id: "002", dept: "admin"},
       {name: "raj", id: "003", dept: "security"}
     ]
     res.render("../views/main",
-      {person: person})
-
+      {me:me})
   }
-}
+    }
