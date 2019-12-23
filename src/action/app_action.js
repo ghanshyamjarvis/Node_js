f31ced4208e0f7f20d007983acfba2f20e38e6e2
USR-08 <ghanshyam.jarvis@gmail.com>
2019-12-23 11:00:12 +0530

    Add new array send table

--- src/action/app_action.js
+++ src/action/app_action.js
@@ -5,9 +5,14 @@ module.exports= {
       {name: "ram", id: "002", dept: "admin"},
       {name: "raj", id: "003", dept: "security"}
     ]
+
+    var you = [{name: "shyam", id: "001", dept: "computer"},
+      {name: "ram", id: "002", dept: "admin"},
+      {name: "raj", id: "003", dept: "security"}
+    ]
     res.render("../views/main",
-      {me: me})
-  }
+      {me:me , you : you})
+  },
 }
 
 
