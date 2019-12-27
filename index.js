048b0cd12b85929c71269178cc930d8b5f60b713
USR-08 <ghanshyam.jarvis@gmail.com>
2019-12-27 11:46:27 +0530

    Curd Opreation with join left and right

--- index.js
+++ index.js
@@ -50,50 +50,87 @@ app.get('/products',(req,res) =>{
 });
 
 // Insert Data into user table using Array
-app.get('/user_tb_data',() => {
-  const record = [[7, 'John', 154],
-                [8,'shyam',155],
-                [9,'Raj',156],
-                [10,'Ramesh',157],
-                [11,'Sita',158]];
+app.get('/user_tb_data',(req,res) => {
+  const record = [[71, 'John', 154],
+                [81,'shyam',155],
+                [91,'Raj',156],
+                [101,'Ramesh',157],
+                [111,'Sita',158]];
     const sql = 'insert into users(id,name,favorite_product) values ?';
     db.query(sql,[record],function (err, result) {
       if (err) throw err;
-      console.log(result)
+      console.log(result);
+      res.send("users table Insert data ");
+});
 });
 
 
 
 //Insert Data into product table
-    app.get('/product_rec',()=>{
-    const record =[[154,'Chocolate Heaven'],
-                    [ 155,'Tasty Lemons' ],
-                    [ 156, 'Vanilla Dreams']
+    app.get('/product_rec',(req,res) => {
+    const record =[[1541,'Chocolate Heaven'],
+                    [ 115,'Tasty Lemons' ],
+                    [ 1156, 'Vanilla Dreams']
                   ];
       const sql = 'insert into products (id,name) values ?';
-      db.query(sql,[record],function (err,res) {
+      db.query(sql,[record],function (err,result) {
       if (err) throw err;
-      console.log(res)
+      console.log(result)
+        res.send("insert data into product table")
     });
   });
 
 // join table
-  app.get('/joinnn',() => {
+  app.get('/joinnn',(req,res) => {
     const sql = "SELECT users.name, products.name FROM users JOIN products ON users.favorite_product = products.id";
     db.query(sql,function (err,result) {
       if (err){
         throw err;
       }
       console.log(result)
+      res.send("Simple Join data where id match in both tables")
     })
-  })
+  });
 
-
-
-
-
-
-
+  //update data into user table
+app.get('/upt_usr_tbl_data/:id',(req,res)=>{
+  const updt_data = "000";
+  const sql = `update users SET '${updt_data}' where id = ${req.params.id}`;
+  db.query(sql,function (err,result) {
+    if (err)throw err;
+    console.log(result);
+    res.send("id value set to null")
+  });
 });
+
+//Left Join
+app.get("/left_join",(req,res)=>{
+  const sql ='select users.name,products.name from users LEFT JOIN products ON users.favorite_product = products.id';
+  db.query(sql,function (err,result) {
+    if (err) throw err;
+    console.log(result)
+    res.send("Left Join of two tables shows all data of id column that also don't match");
+  });
+});
+
+//Right Join
+app.get("/Right_join",(req,res)=>{
+  const sql ='SELECT users.name,\n' +
+    'products.name\n' +
+    'FROM users\n' +
+    'RIGHT JOIN products ON users.favorite_product = products.id';
+  db.query(sql,function (err,result) {
+    if (err)throw err;
+    console.log(result);
+    res.send("Right Join")
+  })
+});
+
+
+
+
+
+
+
 
 
