e49b9f0e8c04d7cc9a91d9a03164a43058a6b341
ghanshyamjarvis <ghanshyam.jarvis@gmail.com>
2020-01-09 12:10:43 +0530

    ejs edit

--- src/app.js
+++ src/app.js
@@ -3,66 +3,76 @@ const router = express.Router();
 const app = express();
 const connection = require("./models/db");
 const route = require("./controllers/app_route");
-const bodyParser = ('body-parser');
 const path = require('path');
+const bodyParser = require('body-parser');
 
+
+app.use(bodyParser.urlencoded({ extended: true }));
 //set views file
 app.set('views', path.join(__dirname,'views'));
 //set view engine
 app.set('view engine', 'ejs');
 
 // get all data
-app.get('/main',(req, res) => {
+app.get('/',(req, res) => {
   let sql = "SELECT * FROM details";
   let query = connection.query(sql, (err, results) => {
     if(err) throw err;
-    res.render('main',{
+        res.render('main',{
       results: results
     });
   });
 });
 
-//route for insert data
-app.post('/save',(req, res) => {
-  let data = {FirstName: req.body.Firstname, LastName: req.body.Lastname, Email: req.body.Email, Password: req.body.Password};
-  let sql = "INSERT INTO details SET ?";
-  let query = connection.query(sql, data,(err, results) => {
-    if(err) throw err;
-    res.redirect('/ ');
+app.get('/add',(req, res) => {
+  res.render('add',{
   });
 });
-//route for update data
-app.post('/update',(req, res) => {
-  let sql = "UPDATE details SET FirstName='"+req.body.FirstName+"', LastName='"+req.body.LastName+"', Password='"+req.body.Password+"' WHERE product_id="+req.body.id;
-  let query = conn.query(sql, (err, results) => {
+
+//route for insert data
+app.post('/add', (req, res) => {
+ // console.log(req.body);
+  let data = {firstname: req.body.firstname,
+             lastname: req.body.lastname,
+             email: req.body.email,
+             password: req.body.password};
+   // console.log("data",data);
+  let sql = "INSERT INTO details SET ?";
+  let query = connection.query(sql, data,(err, results) => {
+    //console.log("sqldata",data);
     if(err) throw err;
     res.redirect('/');
   });
 });
-//route for delete data
-app.post('/delete',(req, res) => {
-  let sql = "DELETE FROM details WHERE Email="+req.body.Email+"";
+
+app.get('/edit',(req, res) => {
+  let sql = "SELECT * FROM details where email = ?";
+  let query = connection.query(sql, req.query.email, (err, results) => {
+    console.log(results);
+    if(err) throw err;
+    res.render('edit',{
+      results: results
+    });
+  });
+});
+
+
+//route for update data
+app.post('/edit',(req, res) => {
+  let sql = "UPDATE details SET firstname='"+req.body.firstname+"', lastname='"+req.body.lastname+"', password='"+req.body.password+"' WHERE email = '"+req.body.email + "'";
   let query = connection.query(sql, (err, results) => {
     if(err) throw err;
     res.redirect('/');
   });
 });
+//route for delete data
+app.get('/delete',(req, res) => {
+  let sql = "DELETE FROM details WHERE email=?";
+  let query = connection.query(sql,req.query.email, (err, results) => {
+    if(err) throw err;
+    res.redirect('/');
+  });
+});
 
-/*
-var urlencodedParser = bodyParser.urlencoded({ extended: false }));
-app.use('/assets', express.static('stuff'));
-app.get('/',function (req,res) {
-    res.render('index')
-});
-app.post('/contact', urlencodedParser, function (req,res) {
-  console.log(req.body);
-  res.render('contact',{qs:req.query})
-});
-app.get('/contact', function (req,res) {
-  res.render('contact', {qs:req.query})
-});
-*/
+
 module.exports = app;
-
-//http://mfikri.com/en/blog/nodejs-mysql-crud
-//https://www.youtube.com/watch?v=QTAYRmMsVCI&list=PL4cUxeGkcC9gcy9lrvMJ75z9maRw4byYp&index=29
