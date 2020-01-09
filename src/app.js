679f854a01bc2095df39ac217ce241ae7147a232
ghanshyamjarvis <ghanshyam.jarvis@gmail.com>
2020-01-09 15:32:56 +0530

    src

--- src/app.js
+++ src/app.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const app = express();
-const connection =require("./models/db");
+//const connection =require("./models/db");
+const path = require('path');
+//const bodyParser = require('body-parser');
+//const sha1 = require ('sha1');
 
-router.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-  res.header('Access-Control-Expose-Headers', 'X-My-Custom-Header, X-Another-Custom-Header');
-  next(); // make sure we go to the next routes and don't stop here
-});
+//app.use(bodyParser.urlencoded({extends:true}));
+//app.use('views',path.join(__dirname,'views'));
+app.set('view engine', 'ejs');
 
-app.use =('/user', require('./controllers/app_route'));
+/*app.get('/',(req,res) => {
+
+})*/
 module.exports = app;
