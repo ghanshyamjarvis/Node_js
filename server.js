485d61cbd23a910460032088cc1be68ef65c8eeb
ghanshyamjarvis <ghanshyam.jarvis@gmail.com>
2020-01-18 17:52:23 +0530

    server

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const app = require('./src/app');
-const port = process.env.PORT || 3000;
+const port  = process.env.PORT || 4000;
 
-const  server = app.listen(port,function () {
-  console.log("Connected At " + port);
+const server = app.listen(port,function () {
+  console.log("Connect To Port" + port);
 });
-
-server.timeout = 30000;
+server.timeout =30000;
