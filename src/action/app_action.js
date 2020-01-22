38aef6c632258df373301c66e2f39e3e4a863996
ghanshyamjarvis <ghanshyam.jarvis@gmail.com>
2020-01-22 17:05:33 +0530

    rename variables

--- src/action/app_action.js
+++ src/action/app_action.js
@@ -1,43 +1,29 @@
-
 const connection = require ('../models/db');
 const UserModel = require('../models/app_model');
 
-
 module.exports={
-
-  //Get All Record  from state table
-  getallstate: function (req, res) {
-    UserModel.checkrecordstate(req.params.country_id).then(async (data) => {
-      res.json({status: true, data: data, message: "shows all record state"})
+  //Get All Record  from state table by country_id
+  state: function (req, res) {
+    UserModel.recordState(req.params.countryId).then(async (data) => {
+      res.json({status: true, data: data})
     })
   },
 //Get All Record  from country table
-  getallcountry: function (req, res) {
-    UserModel.checkrecordcountry().then(async (data) => {
+  country: function (req, res) {
+    UserModel.recordCountry().then(async (data) => {
       res.json({status: true, data: data, message: "shows all record country"})
     })
   },
-
-  getallcities: function (req, res) {
-    UserModel.checkrecordcities(req.params.state_id).then(async (data) => {
+//Get all Record  from cities table by state_id
+  cities: function (req, res) {
+    UserModel.recordCities(req.params.stateId).then(async (data) => {
       res.json({status: true, data: data, message: "shows all record cities"})
     })
   },
-
+//EJS route
   index: function (req, res) {
-    var obj = {};
     res.render('index',{
-      title:'Express'
-
-
+      title:'Express Ajax Display Dorpdown'
     });
   },
-
 }
-
-
-
-
-
-
-
