2ab9de5c9792118b6e92b3bd0fc4c3b26b272e73
ghanshyamjarvis <ghanshyam.jarvis@gmail.com>
2020-01-22 17:09:33 +0530

    change route name

--- src/controllers/app_route.js
+++ src/controllers/app_route.js
@@ -2,11 +2,8 @@ const express =require('express');
 const router = express.Router();
 const UserAction = require('../action/app_action');
 
-
-router.get('/getallstate/:country_id', UserAction.getallstate); //state table route
-router.get('/getallcountry', UserAction.getallcountry);
-router.get('/getallcities/:state_id', UserAction.getallcities);
-router.get('/',UserAction.index)
-
-
+router.get('/states/:countryId', UserAction.state); //state table find by country id route
+router.get('/countries', UserAction.country); // Get all country table data route
+router.get('/cities/:stateId', UserAction.cities);// cities table find by state_id route
+router.get('/',UserAction.index); // index.ejs route
 module.exports = router;
